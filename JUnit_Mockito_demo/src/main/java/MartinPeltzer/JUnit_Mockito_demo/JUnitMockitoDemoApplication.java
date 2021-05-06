b5d1408f5beb1b74081fe19094558ca646d79e12
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-05-06 11:34:37 +0200

    First commit

---
+++ JUnit_Mockito_demo/src/main/java/MartinPeltzer/JUnit_Mockito_demo/JUnitMockitoDemoApplication.java
@@ -0,0 +1,13 @@
+package MartinPeltzer.JUnit_Mockito_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitMockitoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnitMockitoDemoApplication.class, args);
+	}
+
+}
---
+++ JUnit_Mockito_demo/src/test/java/MartinPeltzer/JUnit_Mockito_demo/JUnitMockitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package MartinPeltzer.JUnit_Mockito_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitMockitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
